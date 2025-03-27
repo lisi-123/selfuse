9c52a5e7463b5a8a6aa6385074daf230c1963af4
lisi-123 <160303845+lisi-123@users.noreply.github.com>
2025-03-27 15:19:25 +0800

    Update warp-reinstall.sh

--- warp-reinstall.sh
+++ warp-reinstall.sh
@@ -6,4 +6,7 @@ echo y | warp u
 # 下载warp并建立socks5代理
 wget -N https://gitlab.com/fscarmen/warp/-/raw/main/menu.sh && bash menu.sh <<< $'2\n13\n40000\n1\n'
 
+# warp ipv6优先
+warp s 6
+
 echo "已重装warp并设置socks5代理"
